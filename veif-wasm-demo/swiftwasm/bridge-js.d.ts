28adbaf328ff07321cfa80091797869ab46b0742
Yusuke Hata <42143893+octu0@users.noreply.github.com>
2026-02-20 00:49:00 +0900

    veif wasm demo

---
+++ veif-wasm-demo/swiftwasm/bridge-js.d.ts
@@ -0,0 +1,22 @@
+// NOTICE: This is auto-generated code by BridgeJS from JavaScriptKit,
+// DO NOT EDIT.
+//
+// To update this file, just rebuild your project or run
+// `swift package bridge-js`.
+
+export type Exports = {
+    encodeOne(data: any, width: number, height: number, bitrate: number): any;
+    decodeOne(data: any): any;
+    encode(data: any, width: number, height: number, bitrate: number): any;
+    decode(data: any): any;
+    decodeUpTo(data: any, maxLayer: number): any;
+}
+export type Imports = {
+}
+export function createInstantiator(options: {
+    imports: Imports;
+}, swift: any): Promise<{
+    addImports: (importObject: WebAssembly.Imports) => void;
+    setInstance: (instance: WebAssembly.Instance) => void;
+    createExports: (instance: WebAssembly.Instance) => Exports;
+}>;
