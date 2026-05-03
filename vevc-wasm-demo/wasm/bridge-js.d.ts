0b5fed1ebed717f6abb974167074e3c82be91b6a
Yusuke Hata <42143893+octu0@users.noreply.github.com>
2026-05-03 23:12:33 +0900

    update

---
+++ vevc-wasm-demo/wasm/bridge-js.d.ts
@@ -0,0 +1,23 @@
+// NOTICE: This is auto-generated code by BridgeJS from JavaScriptKit,
+// DO NOT EDIT.
+//
+// To update this file, just rebuild your project or run
+// `swift package bridge-js`.
+
+export type Exports = {
+    createEncoder(width: number, height: number, maxbitrate: number, framerate: number, onChunk: any): any;
+    encodeFrame(id: number, data: any, onDone: any): void;
+    closeEncoder(id: number): void;
+    createDecoder(onFrame: any): any;
+    decodeChunk(id: number, data: any, onDone: any): void;
+    closeDecoder(id: number): void;
+}
+export type Imports = {
+}
+export function createInstantiator(options: {
+    imports: Imports;
+}, swift: any): Promise<{
+    addImports: (importObject: WebAssembly.Imports) => void;
+    setInstance: (instance: WebAssembly.Instance) => void;
+    createExports: (instance: WebAssembly.Instance) => Exports;
+}>;
